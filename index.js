dd234e3d749c08f479d39a31ed183ce79d753340
JesusDaniel97 <102211446+JesusDaniel97@users.noreply.github.com>
2023-11-07 13:05:02 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ let iniciar;
 document.getElementById("iniciar").addEventListener("click",Moverse);
 
 function Moverse(){
-    document.getElementById("Login").style.left="left";
+     document.getElementById("Login").style.bottom="300px";
+     document.getElementById("Login").style.opacity=".80";
+     document.getElementById("registro").style.opacity=".01"
     
 }
