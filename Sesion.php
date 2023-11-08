abac57ba473d1e2986bc95fe849e09d3017103c1
JesusDaniel97 <102211446+JesusDaniel97@users.noreply.github.com>
2023-11-07 21:05:03 -0600

    Update Sesion.php

--- Sesion.php
+++ Sesion.php
@@ -1,13 +1,23 @@
 <?php
- $contraseña = "123456";
- if($_POST["contraseña"] == $contraseña){
-    session_start();
-    $_SESSION["usuario"] = $_POST["usuario"];
-    header("Location:index.php");
-
-}else{
-    header("Location:Registro_Sesion.html");
-}
-
-
-?>
+ try{
+    $base = new PDO("mysql:host=localhost;dbname=directorio","root","");
+    $base->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    $sql="SELECT*FROM registro WHERE Usuario=:usuario AND Contrasenia=:contrasenia";
+    $resultado = $base->prepare($sql);
+    $usuario=htmlentities(addslashes($_POST["usuario"]));
+    $contraseña=htmlentities(addslashes($_POST["contrasenia"]));
+    $resultado->bindValue("usuario",$usuario);
+    $resultado->bindValue("contrasenia",$contraseña);
+    $resultado->execute();
+    $numero_registro=$resultado->rowCount();
+    if($numero_registro!=0){
+        session_start();
+        $_SESSION["usuario"] = $_POST["usuario"];
+        header("Location:index.php");
+    
+    }else{
+        header("Location:Registro_Sesion.html");
+    }
+ }catch(Exception $e){
+     die("Error:". $e -> getMessage());
+ }
