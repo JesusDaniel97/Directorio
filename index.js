16c1428d053f37ef264c1ac86e3c9270c0874ffb
JesusDaniel97 <102211446+JesusDaniel97@users.noreply.github.com>
2023-11-13 11:11:56 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 let iniciar;
 document.getElementById("iniciar").addEventListener("click",Moverse);
+document.getElementById("Sesion").addEventListener("click",ValidarDatos);
+document.getElementById("Registrarse").addEventListener("click", function() {
+     alert("Registrado correctamente!");
 
 function Moverse(){
      document.getElementById("Login").style.bottom="300px";
      document.getElementById("Login").style.opacity=".80";
      document.getElementById("registro").style.opacity=".01"
-    
+     
 }
+
+function ValidarDatos(){
+     let usuario = document.forms["login"]["name"].value;
+     let contrasenia = document.forms["login"]["contrasenia"].value
+
+     if(usuario != "DavidCamacho" || contrasenia != 14789){
+          window.alert("contraseña incorrecta ");
+     }
+}
+                                                        
